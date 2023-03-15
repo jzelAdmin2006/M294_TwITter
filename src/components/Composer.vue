b23aaf7381733412a5c8dd8211d346022aa68e0b
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-03-15 19:35:27 +0100

    solve D1

--- src/components/Composer.vue
+++ src/components/Composer.vue
@@ -1,13 +1,20 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const tweetText = ref('')
+
+const tweetTextLength = computed(() => tweetText.value.length)
+</script>
 <template>
     <form class="composer">
         <label class="composer__prompt">Was geht?</label>
-        <textarea maxlength="160" class="composer__textarea" placeholder="Verfasse einen Tweet..." />
+        <textarea maxlength="160" class="composer__textarea" placeholder="Verfasse einen Tweet..." v-model="tweetText" />
         <div class="composer__actions">
             <div class="composer__stats stats">
-                <span class="stats__counter">0</span>
+                <span class="stats__counter">{{ tweetTextLength }}</span>
                 <span class="stats__max">/ 160</span>
             </div>
-            <button disabled class="btn btn--primary">
+            <button :disabled="tweetTextLength < 5" class="btn btn--primary">
                 Tweet veröffentlichen
             </button>
         </div>
