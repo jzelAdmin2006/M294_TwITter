f504c2abd86834246c919da1dd1738e2d0a1995a
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-03-15 20:23:06 +0100

    solve E1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,16 @@
 import LoginInfo from '../components/LoginInfo.vue'
 import Composer from '../components/Composer.vue'
 import Tweet from '../components/Tweet.vue'
+import { ref, onMounted } from 'vue'
+
+const loading = ref(true)
+
+onMounted(async () => {
+    console.log('HomeView mounted')
+    setTimeout(() => {
+        loading.value = false
+    }, 3000)
+})
 </script>
 
 <template>
@@ -9,7 +19,10 @@ import Tweet from '../components/Tweet.vue'
     <LoginInfo />
     <Composer />
     <!-- Stream -->
-    <section class="stream">
+    <div class="loading" v-if="loading">
+        Lade Tweets...
+    </div>
+    <section class="stream" v-else>
         <Tweet v-for="tweet in [1, 2, 3, 4, 5, 6, 7, 8, 9]" :user="{ id: 1, name: 'John Doe' }"
             :text="'Lorem ipsum dolor med'" :created-at="'2023-01-01T12:00:00Z'" />
     </section>
