463bc7a57a9984c7ceb6f0c65703153f245f2169
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-03-16 12:53:03 +0100

    improve I4

--- src/App.vue
+++ src/App.vue
@@ -60,22 +60,6 @@ body {
   grid-template-columns: 220px auto 260px;
 }
 
-.logo {
-  font-size: 2.6rem;
-  font-weight: 500;
-  padding: 20px 0;
-
-  background: linear-gradient(to bottom, #fb7185, #e11d48);
-  background-clip: text;
-  text-fill-color: transparent;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-
-.logo strong {
-  font-weight: 800;
-}
-
 .content {
   min-height: 100vh;
   border-right: 1px solid #1e293b;
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,35 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goToHome() {
+    router.push('/')
+}
+</script>
+
 <template>
-    <a href="/">
-        <div class="logo">
-            Tw<strong>IT</strong>ter
-        </div>
-    </a>
+    <div class="logo" @click="goToHome">
+        Tw<strong>IT</strong>ter
+    </div>
 </template>
+
+
+<style>
+.logo {
+    font-size: 2.6rem;
+    font-weight: 500;
+    padding: 20px 0;
+
+    background: linear-gradient(to bottom, #fb7185, #e11d48);
+    background-clip: text;
+    text-fill-color: transparent;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    cursor: pointer;
+}
+
+.logo strong {
+    font-weight: 800;
+}
+</style>
